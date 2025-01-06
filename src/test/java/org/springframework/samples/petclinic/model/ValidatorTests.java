7da0741e3b4619016c33eba2f5c2fd2171794687
stubrowncloudbees <33249923+stubrowncloudbees@users.noreply.github.com>
2025-01-06 15:20:24 +0000

    Update ValidatorTests.java

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -53,7 +53,7 @@ class ValidatorTests {
 
 		assertThat(constraintViolations).hasSize(1);
 		ConstraintViolation<Person> violation = constraintViolations.iterator().next();
-		assertThat(violation.getPropertyPath()).hasToString("firstName");
+		assertThat(violation.getPropertyPath()).hasToString("first1Name");
 		assertThat(violation.getMessage()).isEqualTo("must not be blank");
 	}
 
